721fd389aae89fd91b7e3074021010fa2534c0ba
Jiles Smith <jjilescoll2022ful@csu.fullerton.edu>
2025-12-07 16:07:02 -0800

    added a tasks_queeu consuming worked job being number addition by its provided integer in the input

--- app/worker.py
+++ app/worker.py
@@ -1,1 +1,15 @@
 #processes jobs
+#workers consume job from task_queue
+#producers push to task queue
+
+import json
+import client_redis import get_redis
+import time
+
+r = get_redis()
+QUEUE_KEY = "task_queue"
+
+def do_work(job): #task here to add number provided by itself
+    x = job["x"]
+    return {"id" : job["id"], "result" : x + x}
+
