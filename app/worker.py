801e37374b0d615d58eb5c28de2fc6ccd00f945a
Jiles Smith <jjilescoll2022ful@csu.fullerton.edu>
2025-12-07 16:19:36 -0800

    file that consums jobs from tasks created and loop formed to subtract tasks from queue and complete or fail them with proper error handling consisting of try and except blocks

--- app/worker.py
+++ app/worker.py
@@ -13,3 +13,24 @@ def do_work(job): #task here to add number provided by itself
     x = job["x"]
     return {"id" : job["id"], "result" : x + x}
 
+def cache_result(job_id,value,ttl=120): #lasts 120 seconds
+    r.setex(f"result:{job_id}", ttl, json.dumps(value))
+
+def main():
+    print("[WORKER READY] Waiting for task... ;)")
+    while True:
+        _, job_raw = r.brpop(QUEUE_KEY, timeout = 0)
+        job = json.loads(job_raw)
+        job_id = job["id"]
+
+        r.publish("events", json.dumps({"type" : "job.started", "job_id" : job_id}))
+        try:
+            result = do_work(job)
+            cache_result(job_id,result)
+            r.publish("events", json.dumps("type" : "job.succeeded", "job_id": job_id))
+            print(f"[DONE] {job_id} -> {result}")
+        except exception as e:
+            r.publish("events",json.dumps({"type" : "job.failed", "job_id" : job_id}))#"type "is a key, "jobfailed" is a string liter, "job_id" is a key, and job_id is a var for value
+            print(f"[ERROR] {job_id} : {e}")
+    if __name__ == "__main__":
+        main()
